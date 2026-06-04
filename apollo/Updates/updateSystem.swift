d5642214e5cb78c82301fa584f331a983df79e17
Matthew Nakhel <amo@Matthews-MacBook-Pro.local>
2026-06-04 12:31:19 -0400

    create app versions and .dmg

---
+++ apollo/Updates/updateSystem.swift
@@ -0,0 +1,18 @@
+//
+//  updateSystem.swift
+//  apollo
+//
+//  Created by Matthew Nakhel on 6/4/26.
+//
+
+import SwiftUI
+
+struct updateSystem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    updateSystem()
+}
