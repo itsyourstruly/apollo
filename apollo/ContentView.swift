53e9ba65f152e5a8b2fc1027ff8d9d93096abcf0
Matthew Nakhel <amo@Mac.lan>
2026-06-05 13:16:56 -0700

    taketoomany

--- apollo/ContentView.swift
+++ apollo/ContentView.swift
@@ -4998,7 +4998,7 @@ struct UnifiedNotchContainer: View {
             title = "Clip"
             symbol = "doc.on.clipboard"
         case .jot:
-            title = "J"
+            title = "Jot"
             symbol = "note.text"
         case .box:
             title = "Box"
